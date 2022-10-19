fbde97e5482e4c6d16a42aa254ed35b3df1e51b4
pipiloveslife <Pain_Zzz@163.com>
2022-10-19 07:41:15 +0000

    Update at 2022-10-19T07:41:15.860090Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -11,6 +11,16 @@ public class Home {
         return cats;
     }
 
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("Home{cats=[");
+        for (Cat cat : cats) {
+            builder.append(cat).append(",");
+        }
+        builder.replace(builder.length() - 1, builder.length(), "]}");
+        return builder.toString();
+    }
+
     public static void main() {
         Home home = new Home(new Cat[] {new Cat("Tom"), new Cat("Jerry")});
         // It should print "Home{cats=[Tom,Jerry]}"

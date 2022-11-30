778a4da7eb3b0b85669d61c9536dc98b8e08c57e
jenny-2233 <822204051@qq.com>
2022-11-30 03:15:16 +0000

    Update at 2022-11-30T03:15:16.909258Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,13 +3,13 @@ package com.bytelegend;
 public class Cat {
     private final String name;
 
+    @Override
+    public String toString() {
+        return name;
+    }
+
     public Cat(String name) {
         this.name = name;
     }
-    @Override
-    public String toString() {
-        return "Cat{" +
-                "name='" + name + '\'' +
-                '}';
-    }
+
 }

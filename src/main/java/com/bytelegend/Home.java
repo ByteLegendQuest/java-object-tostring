f0ac32f915fe3903a43dfcd006f64f8bd9a0b4a2
guakun01 <guakun01@gmail.com>
2023-03-28 14:57:51 +0000

    Update at 2023-03-28T14:57:51.411623Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,5 +1,8 @@
 package com.bytelegend;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Home {
     private final Cat[] cats;
 
@@ -11,8 +14,14 @@ public class Home {
         return cats;
     }
 
+    @Override
+    public String toString() {
+        return String.format("Home{cats=%s}",
+                Arrays.asList(getCats()).stream().map(Cat::toString).collect(Collectors.toList()).toString());
+    }
+
     public static void main() {
-        Home home = new Home(new Cat[] {new Cat("Tom"), new Cat("Jerry")});
+        Home home = new Home(new Cat[] { new Cat("Tom"), new Cat("Jerry") });
         // It should print "Home{cats=[Tom,Jerry]}"
         System.out.println(home);
     }

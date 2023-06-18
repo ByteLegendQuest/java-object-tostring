f4d76c2d7cce47906921ea52c8b446835f67edc2
HumansDoNotWantImmortality <rosinski.krzysztof.01@gmail.com>
2023-06-18 12:51:59 +0000

    Update at 2023-06-18T12:51:59.749849Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,5 +1,7 @@
 package com.bytelegend;
 
+import java.util.Arrays;
+
 public class Home {
     private final Cat[] cats;
 
@@ -11,9 +13,15 @@ public class Home {
         return cats;
     }
 
+    @Override
+    public String toString() {
+        return "Home{" +
+                "cats=" + Arrays.toString(cats) +
+                '}';
+    }
+
     public static void main() {
         Home home = new Home(new Cat[] {new Cat("Tom"), new Cat("Jerry")});
-        // It should print "Home{cats=[Tom,Jerry]}"
         System.out.println(home);
     }
 }
